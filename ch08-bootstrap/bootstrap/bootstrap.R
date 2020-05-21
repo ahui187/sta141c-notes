3456a018bf1feb3c026bc0e31e8b2812b030b225
Randy Lai <randy.cs.lai@gmail.com>
2020-05-21 11:40:46 -0700

    updated to use unlist in allgather

--- ch08-bootstrap/bootstrap/bootstrap.R
+++ ch08-bootstrap/bootstrap/bootstrap.R
@@ -6,7 +6,7 @@ init()
 .comm.rank <- comm.rank()
 
 B <- 10000
-rs <- pbdLapply(seq_len(B), function(i) {
+rs <- pbdSapply(seq_len(B), function(i) {
         n <- nrow(mtcars)
         index <- sample.int(n, n, replace = TRUE)
         x <- mtcars$mpg[index]
@@ -16,13 +16,13 @@ rs <- pbdLapply(seq_len(B), function(i) {
     pbd.mode = "spmd")
 
 
-rslist <- allgather(rs)
+rs <- allgather(rs, unlist = TRUE)
 
 if (.comm.rank == 0) {
-    answer <- quantile(unlist(rslist), c(0.025, 0.975))
+    answer <- quantile(rs, c(0.025, 0.975))
 }
 
-# it is important to not to put the comm.print inside the if loop,
+# it is important to not to put the comm.print inside the if statement,
 # otherwise it will block the runtime
 comm.print(answer)
 
